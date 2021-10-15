7ee901edeace935a19ac83a7ce3d14839a568677
ggjulio <juligonz@student.42.fr>
2021-10-15 13:25:09 +0200

    reverse iterator wip

--- includes/iterator.hpp
+++ includes/iterator.hpp
@@ -110,6 +110,23 @@ template <class Iterator>
   bool operator>= (const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs)
   { return lhs.base() <= rhs.base();}
 
+
+template< class Iterator >
+	reverse_iterator<Iterator> operator+(
+		typename reverse_iterator<Iterator>::difference_type n,
+        const reverse_iterator<Iterator>& it )
+	{
+		    return reverse_iterator<_Iter>(it.base() - n);
+	}
+
+template <class Iterator>
+  typename reverse_iterator<Iterator>::difference_type operator- (
+    	const reverse_iterator<Iterator>& lhs,
+    	const reverse_iterator<Iterator>& rhs)
+	{
+		rhs.base() - lhs.base();
+	}
+
 } /* namespace ft */
 
 #endif /* ITERATOR */

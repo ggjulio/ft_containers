4d9e29f61f3a5e7845330bc874acaaa44fdceabb
ggjulio <juligonz@student.42.fr>
2021-10-16 13:02:40 +0200

    reverse iterator wip

--- tests/src/TestsIterator.cpp
+++ tests/src/TestsIterator.cpp
@@ -37,25 +37,45 @@ TEST_CASE( "cn::iterator_traits", "[iterator_traits]" ) {
 // 	}
 // }
 
+TEST_CASE( "cn::reverse_iterator - basic loop", "[reverse_iterator][basic_loop]" )
+{
+	std::vector<int> vec = {1,2,3,4,5};
+	typedef std::reverse_iterator<std::vector<int>::iterator> reverse_iter;
+
+	reverse_iter it(vec.end());
+	reverse_iter end(vec.begin());
+
+	int n = 5;
+	while (it != end)
+	{
+		CHECK(*it == n);
+		++it;
+		--n;
+		REQUIRE( n >= 0);
+	}
+	REQUIRE( *--it == 1);
+}
+
+
 TEST_CASE( "cn::reverse_iterator", "[reverse_iterator]" ) {
-	std::vector<int> vec = {1,2,4,5,6};
+	std::vector<int> vec = {1,2,3,4,5};
 
 typedef std::reverse_iterator<std::vector<int>::iterator> reverse_iter;
 
 	reverse_iter it(vec.end());
 	reverse_iter end(vec.begin());
 
-	CHECK( *it == 6 );
-	CHECK( *++it == 5 );
+	CHECK( *it == 5 );
 	CHECK( *++it == 4 );
+	CHECK( *++it == 3 );
 	CHECK( *++it == 2 );
 	CHECK( *++it == 1 );
 	CHECK( *--it == 2 );
 	CHECK( *++it == 1 );
-	// CHECK( it.base() == end.base() );
+	CHECK( ++it == end );
 
 	it = reverse_iter(vec.end());
-	REQUIRE( *it == 6 );
+	REQUIRE( *it == 5 );
 	
 	// while
 

ab2dcb75372ca3ad222d03f16ee08bd73ed330f4
ggjulio <juligonz@student.42.fr>
2021-10-21 11:45:58 +0200

    done: identity + select1st tests

--- tests/src/TestsFunction.cpp
+++ tests/src/TestsFunction.cpp
@@ -9,32 +9,24 @@
 #include <vector>
 
 TEMPLATE_TEST_CASE( "cn::_Identity",
-	"[function][identity]", std::string, int, const int)
+	"[function][identity]", int, const int, std::string)
 {
-	typedef typename TestType type_test;
-	typedef	typename const TestType  const_type_test;
+	typedef typename ft::_Identity<TestType> 			ident;
 
-	typedef typename ft::_Identity<type_test> 			ident;
-	typedef typename ft::_Identity<const type_test> 	ident_const;
-
-	CHECK(		typeid(ident::argument_type).hash_code() == typeid(type_test).hash_code()  );
-	CHECK(		typeid(ident::result_type).hash_code() == typeid(type_test).hash_code()  );
-	REQUIRE(	typeid(ident::result_type).name() == typeid(type_test).name()  );
-
-	CHECK(		typeid(ident_const::argument_type).hash_code() == typeid(const_type_test).hash_code()  );
-	CHECK(		typeid(ident_const::result_type).hash_code() == typeid(const_type_test).hash_code()  );
-	REQUIRE(	typeid(ident_const::result_type).name() == typeid(const_type_test).name()  );
-
-
+	CHECK(	typeid(typename ident::argument_type).hash_code() == typeid(TestType).hash_code()  );
+	CHECK(	typeid(typename ident::result_type).hash_code() == typeid(TestType).hash_code()  );
+	CHECK(	typeid(typename ident::result_type).name() == typeid(TestType).name()  );
 }
 
 TEST_CASE( "cn::_Select1st",
 	"[function][select1st]")
 {
 	typedef std::pair<short, long>		some_pair;
-	typedef ft::_Select1st<some_pair> 	ident;
+	typedef ft::_Select1st<some_pair> 	ident_pair;
 
-	ident t;
-	(void)t;
+	CHECK(	typeid(typename ident_pair::argument_type).hash_code() == typeid(some_pair).hash_code()  );
+
+	CHECK(	typeid(typename ident_pair::result_type).hash_code() == typeid(short).hash_code()  );
+	CHECK(	typeid(typename ident_pair::result_type).name() == typeid(short).name()  );
 
 }

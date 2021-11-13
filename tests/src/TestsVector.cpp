0b94710db5fc7ef09da34c9f5d0f74ade25fc770
ggjulio <juligonz@student.42.fr>
2021-11-13 22:09:18 +0100

    vector: add tests for capacity

--- tests/src/TestsVector.cpp
+++ tests/src/TestsVector.cpp
@@ -186,40 +186,36 @@ TEST_CASE( "vector - capacity ", "[vector][capacity]" )
 	}
 }
 
+TEST_CASE( "vector - capacity - vectors can be sized and resized",
+	"[vector][capacity]" )
+{
+	cn::vector<int> v( 5 );
+	REQUIRE( v.size() == 5 );
+	REQUIRE( v.capacity() >= 5 );
 
+	SECTION("resizing bigger changes size and capacity") {
+		v.resize( 10 );
 
+		REQUIRE( v.size() == 10 );
+		REQUIRE( v.capacity() >= 10 );
+	}
+	SECTION("resizing smaller changes size but not capacity") {
+		v.resize( 0 );
 
+		REQUIRE( v.size() == 0 );
+		REQUIRE( v.capacity() >= 5 );
+	}
+	SECTION("We can use the 'swap trick' to reset the capacity") {
+		cn::vector<int> empty;
+		empty.swap( v );
 
-
-
-// TEST_CASE( "vectors can be sized and resized", "[vector][template][size()][capacity]" ) {
-
-	// cn::vector<int> v( 5 );
-	// REQUIRE( v.size() == 5 );
-	// REQUIRE( v.capacity() >= 5 );
-
-	// SECTION("resizing bigger changes size and capacity") {
-	//     v.resize( 10 );
-
-	//     REQUIRE( v.size() == 10 );
-	//     REQUIRE( v.capacity() >= 10 );
-	// }
-	// SECTION("resizing smaller changes size but not capacity") {
-	//     v.resize( 0 );
-
-	//     REQUIRE( v.size() == 0 );
-	//     REQUIRE( v.capacity() >= 5 );
-	// }
-	// SECTION("We can use the 'swap trick' to reset the capacity") {
-	//     cn::vector<int> empty;
-	//     empty.swap( v );
-
-	//     REQUIRE( v.capacity() == 0 );
-	// }
-	// SECTION("reserving smaller does not change size or capacity") {
-	//     v.reserve( 0 );
+		REQUIRE( v.capacity() == 0 );
+	}
+	SECTION("reserving smaller does not change size or capacity") {
+		v.reserve( 0 );
 		
-	//     REQUIRE( v.size() == 5 );
-	//     REQUIRE( v.capacity() >= 5 );
-	// }
-// }
+		REQUIRE( v.size() == 5 );
+		REQUIRE( v.capacity() >= 5 );
+	}
+}
+
